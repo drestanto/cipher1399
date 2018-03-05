85bd87815066067a5cc78df6538b05e419889bb2
drestanto <dyas@live.com>
2018-03-05 13:35:47 +0700

    generate key

--- Cipher1399.py
+++ Cipher1399.py
@@ -5,8 +5,23 @@ class Cipher1399 :
 
 	def __init__(self, key, filein):
 		self.key = key
+		self.round_key = ""
 		self.filein = filein
 		self.s_box = []
+
+	@staticmethod
+	def get_round_key(key, length):
+		random.seed(key)
+		round_key = ""
+		for i in range(0,length):
+			round_key += str(chr(random.randint(0,255)))
+		return round_key
+
+	def get_first_round_key(self):
+		self.round_key = Cipher1399.get_round_key(self.key,3)
+
+	def get_next_round_key(self):
+		self.round_key = Cipher1399.get_round_key(self.round_key,3)
 
 	def read_block_by_block(self):
 		file = open(self.filein, 'rb')
@@ -19,13 +34,19 @@ class Cipher1399 :
 		# block harus berukuran 64 bit
 		pass
 
-	def make_s_box(self):
+	def make_s_box(self): # make sure the self.round_key is correct at this step
 		# initiate s-box
-		for i in range(0,15):
+		s_box_size = 16
+		for i in range(0,s_box_size):
 			arr_baris = []
-			for j in range(0,15):
+			for j in range(0,s_box_size):
 				arr_baris.append(-1)
 			self.s_box.append(arr_baris)
+
+		# make s-box from round_key
+		print(self.s_box[0][15])
+
+
 
 	def print_s_box(self):
 		for arr in self.s_box:
@@ -38,5 +59,6 @@ class Cipher1399 :
 ciph = Cipher1399("test","datatest.txt")
 print(ciph.key)
 ciph.read_block_by_block()
+ciph.get_first_round_key()
 ciph.make_s_box()
-ciph.print_s_box()
+# ciph.print_s_box()
