3c418e624da7bc1210ed61cde86a7b0826b3eb64
drestanto <dyas@live.com>
2018-03-05 13:03:01 +0700

    read block by block (64 bit)

--- Cipher1399.py
+++ Cipher1399.py
@@ -2,9 +2,24 @@ import random
 
 class Cipher1399 :
 
-	def __init__(self, key):
+	def __init__(self, key, filein):
 		self.key = key
+		self.filein = filein
+
+	def read_block_by_block(self):
+		file = open(self.filein, 'rb')
+		block = file.read(8)
+		while (block != ""):
+			print(block)
+			block = file.read(8)
+
+	def process_block(block):
+		# block harus berukuran 64 bit
+		pass
 
 
-ciph = Cipher1399("test")
+
+
+ciph = Cipher1399("test","datatest.txt")
 print(ciph.key)
+ciph.read_block_by_block()
