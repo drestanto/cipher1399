29505b8f1cb667e6ba4f1a14794f24e39dd7c2aa
drestanto <dyas@live.com>
2018-03-05 13:16:52 +0700

    init s-box

--- Cipher1399.py
+++ Cipher1399.py
@@ -1,10 +1,12 @@
 import random
+import sys
 
 class Cipher1399 :
 
 	def __init__(self, key, filein):
 		self.key = key
 		self.filein = filein
+		self.s_box = []
 
 	def read_block_by_block(self):
 		file = open(self.filein, 'rb')
@@ -17,9 +19,24 @@ class Cipher1399 :
 		# block harus berukuran 64 bit
 		pass
 
+	def make_s_box(self):
+		# initiate s-box
+		for i in range(0,15):
+			arr_baris = []
+			for j in range(0,15):
+				arr_baris.append(-1)
+			self.s_box.append(arr_baris)
 
+	def print_s_box(self):
+		for arr in self.s_box:
+			for elem in arr:
+				sys.stdout.write(format(elem,'02x'))
+				sys.stdout.write(' ')
+			print("")
 
 
 ciph = Cipher1399("test","datatest.txt")
 print(ciph.key)
 ciph.read_block_by_block()
+ciph.make_s_box()
+ciph.print_s_box()
